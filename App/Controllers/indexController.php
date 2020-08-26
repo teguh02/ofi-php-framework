bd0a57829b4a840b79993d001781d500db36e616
teguh02 <teguhrijanandi02@gmail.com>
2020-08-26 16:09:34 +0700

    Add HttpSupport

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -7,6 +7,7 @@ use App\Core\helper as h; // Untuk mempersingkat nama class
 use Illuminate\Database\Capsule\Manager as DB;
 use vendor\OFI_PHP_Framework\Controller;
 use vendor\OFI_PHP_Framework\Controller\Route;
+use vendor\OFI_PHP_Framework\Support\HttpSupport;
 
 class indexController extends Controller
 {
@@ -75,6 +76,75 @@ class indexController extends Controller
         echo "<pre>";
         print_r($route);
         echo "<pre>";
+    }
+
+    /**
+     * Example when you want to GET or POST data to other API
+     * server
+     */
+
+    public function Http()
+    {
+        // In this code sample
+        // I'm use https://webhook.site/ as REST API Cathcer
+
+        $url = "https://webhook.site/5adf5298-4637-437c-b220-c14854de0ef7?app=ofi%20php%20framework";
+
+        $http = new HttpSupport();
+
+        // GET Code Example
+        $get = $http -> method('GET') -> url($url) -> execute();
+
+        // POST Code example
+
+        $post = $http -> method("POST") 
+                -> url($url)
+                
+                // Header as array
+                ->header([
+                    'App: OFI PHP Framework',
+                    'key: 123'
+                ])
+
+                // Body as Array
+                -> body([
+                    'App'       => 'OFI PHP Framework',
+                    'Author'    => 'Teguh Rijanandi'
+                ]) -> execute();
+
+        // PUT Code Example
+
+        // $put = $http -> method("PATCH") 
+        $put = $http -> method("PUT") 
+                -> url($url)
+                ->header([
+                    'App: OFI PHP Framework',
+                    'key: 123'
+                ])
+                -> body([
+                    'App'       => 'New Data : OFI PHP Framework',
+                    'Author'    => 'New Data : Teguh Rijanandi'
+                ]) -> execute();
+
+        // DELETE Code Method
+        $delete = $http -> method("DELETE") 
+                -> url($url . '&id=1')
+                ->header([
+                    'App: OFI PHP Framework',
+                    'key: 123'
+                ])
+                -> body([
+                    'App'       => 'New Data : OFI PHP Framework',
+                    'Author'    => 'New Data : Teguh Rijanandi'
+                ])  -> execute();
+
+        // Print the result
+        
+        // print_r($get);
+
+        // print_r($post);
+
+        print_r($delete);
     }
 
     public function uploadImage()
