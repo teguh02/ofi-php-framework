d3dd4547fe7b5e28b6242dd194d3595dd352f1f8
teguh02 <teguhrijanandi02@gmail.com>
2020-08-27 13:44:04 +0700

    Major update 6

--- App/Middleware/cors.php
+++ App/Middleware/cors.php
@@ -14,7 +14,7 @@ class cors {
     {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Credentials', true);
-        header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        header('Access-Control-Allow-Methods', 'GET, POST');
         header('Access-Control-Allow-Headers', '*');
     }
 }
