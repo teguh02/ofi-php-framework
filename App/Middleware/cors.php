7efff863ffd39ee5e972bebb5ecbb5abfc14ffe2
teguh02 <teguhrijanandi02@gmail.com>
2021-01-25 09:05:59 +0700

    updated to v3.9.6

--- App/Middleware/cors.php
+++ App/Middleware/cors.php
@@ -14,7 +14,7 @@ class cors {
     {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Credentials', true);
-        header('Access-Control-Allow-Methods', 'GET, POST');
+        header('Access-Control-Allow-Methods', '*');
         header('Access-Control-Allow-Headers', '*');
     }
 }
